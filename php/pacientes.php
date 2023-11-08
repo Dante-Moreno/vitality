90765a2e390da3c8d5666cc30da7fe3abf1bfdbe
agusvega17 <gabrielagustinvega@gmail.com>
2023-11-08 19:30:14 -0300

    cambios

--- php/pacientes.php
+++ php/pacientes.php
@@ -8,8 +8,8 @@
     <title>Document</title>
 </head>
 <body>
-    <main>
-    <div class="menu">
+     <!--<main>
+   <div class="menu">
         <div class="logo">
             <img src="tu_logo.png" alt="Logo del consultorio">
         </div>
@@ -19,14 +19,21 @@
             <li><a href="#">Pacientes</a></li>
         </ul>
     </div>
-    </main>
+    </main>-->
 =======
     
     <title>Document</title>
 </head>
 <body>
 
->>>>>>> 
+ 
+<nav class="navbar navbar-light bg-light">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
 
+>>>>>>>
 </body>
 </html>
