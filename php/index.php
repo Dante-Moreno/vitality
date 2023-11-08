8530723690a04dff64f839751e5d1d66434e17a7
Dante-Moreno <dantemmoreno100@gmail.com>
2023-11-08 18:36:53 -0300

    home

---
+++ php/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+    <div class="menu">
+        <div class="logo">
+            <img src="tu_logo.png" alt="Logo del consultorio">
+        </div>
+        <ul class="menu-items">
+            <li><a href="#">Turnos</a></li>
+            <li><a href="#">Calendario</a></li>
+            <li><a href="#">Pacientes</a></li>
+        </ul>
+    </div>
+    </main>
+</body>
+</html>
